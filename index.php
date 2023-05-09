f30badd78d24847021f287203b0fd5d767c19206
Marcelin <marcelintingougoui3@gmail.com>
2023-05-09 10:43:55 +0200

    💄 restructuration de l'index.html

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include 'Partials/_head.html.php'  ?>
+    
+            <h1 class="big-text">Trouve les informations sur n'importe quel pays.</h1>
+            <div class="cards">
+                <a href="/world.php" class="card">
+                    <h3>Mondiale</h3>
+                </a>
+                <a href="#" class="card">
+                    <h3>Continent</h3>
+                </a>
+                <a href="#" class="card">
+                    <h3>Pays</h3>
+                </a>
+            </div>
+        </main>
+<?php include 'Partials/_footer.html.php'  ?>
+       
